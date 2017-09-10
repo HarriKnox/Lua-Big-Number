2eb5b2a975a825f8953250f48244b9cc256a390e
Harrison Patrick Knox <harri.alpha96@gmail.com>
2017-09-10 11:06:02 -0700

    Undo the "optimization" changes that broke things

--- biginteger.lua
+++ biginteger.lua
@@ -501,36 +501,31 @@ end
 
 function getleadingzeros(int)
    -- Returns the number of leading zeros in the 32-bit integer.
-   -- Uses Hacker's Delight method used by Java Integer
+   -- Uses Hacker's Delight figure 5-6 method used by Java Integer
    local n = 1
-   local s
    
    if int == 0 then
       return 32
    end
    
-   s = bitrightshift(int, 16)
-   if s == 0 then
+   if bitrightshift(int, 16) == 0 then
       n = n + 16
-      int = s
+      int = bitleftshift(int, 16)
    end
    
-   s = bitrightshift(int, 24)
-   if s == 0 then
+   if bitrightshift(int, 24) == 0 then
       n = n + 8
-      int = s
+      int = bitleftshift(int, 8)
    end
    
-   s = bitrightshift(int, 28)
-   if s == 0 then
+   if bitrightshift(int, 28) == 0 then
       n = n + 4
-      int = s
+      int = bitleftshift(int, 4)
    end
    
-   s = bitrightshift(int, 30)
-   if s == 0 then
+   if bitrightshift(int, 30) == 0 then
       n = n + 2
-      int = s
+      int = bitleftshift(int, 2)
    end
    
    return n - bitrightshift(int, 31)
